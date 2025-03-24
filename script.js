7653d2f17274fcb828aa3aab78d5a4af20271fd1
Vansh Chauhan <vanshc0506@gmail.com>
2025-03-24 19:17:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const counterDisplay = document.getElementById('counter');
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    count++;
+    counterDisplay.textContent = count;
+});
